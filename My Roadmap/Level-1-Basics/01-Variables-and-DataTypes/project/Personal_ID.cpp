c3f676493410b04ba326aa21e3922e41b3a06ef1
mohdrashadi-pro <moalrashadi@gmail.com>
2026-03-01 13:05:19 +0800

    feat: complete Personal ID Card Generator

--- My Roadmap/Level-1-Basics/01-Variables-and-DataTypes/project/Personal_ID.cpp
+++ My Roadmap/Level-1-Basics/01-Variables-and-DataTypes/project/Personal_ID.cpp
@@ -1,14 +1,38 @@
-//
-// Created by MOHAMMED ALRASHADI on 01/03/2026.
-//
+
 
 #include "Personal_ID.h"
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
 
 int main () {
+string name = "Alice Johnson";
+    int age = 20;
+    float height = 1.65;
+    double gpa = 3.85;
+    char grade = 'A';
+    bool isStudent = true;
+
+    // output or  print
+
+    cout << "=========================" << endl;
+    cout << "     STUDENT ID CARD     " << endl;
+    cout << "=========================" << endl;
+
+
+    cout << left << setw(10) << "Name"<< ": " << name << endl;
+    cout << left << setw(10) <<"Age"  <<": " <<age << endl;
+    cout <<left << setw(10) <<"Height" <<": " <<height<<endl;
+    cout <<left <<setw(10) <<"GPA" <<": " <<gpa<<endl;
+
+    if (isStudent) {
+        cout << left << setw(10) <<"Student" <<": true" <<endl;
+    }
+
+
+
 
 
 
