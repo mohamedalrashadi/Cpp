cb629405f6d05d35eeb75ee3ab0a1b13785e9a8a
mohdrashadi-pro <moalrashadi@gmail.com>
2026-03-01 12:20:29 +0800

    start C++ journal — Level 1: Basic Variables & Data Types
    - Create practice folder: variables-and-data-types/
    - Add first project: Personal ID Card Generator
    - Add pseudocode for all projects in this chaptereate pseudocode ,
    it's all pesudcode for all project in variable and datatype

---
+++ My Roadmap/Level-1-Basics/01-Variables-and-DataTypes/project/Personal_ID.cpp
@@ -0,0 +1,16 @@
+//
+// Created by MOHAMMED ALRASHADI on 01/03/2026.
+//
+
+#include "Personal_ID.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main () {
+
+
+
+    return 0;
+}
